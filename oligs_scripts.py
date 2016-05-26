9676b0ef2507a1d7d6c3ffee6cfa5fae00f02167
Rail <rail.suleymanov@gmail.com>
2016-05-26 14:39:41 +0300

    Added more oligs tests

--- oligs_scripts.py
+++ oligs_scripts.py
@@ -1,12 +1,14 @@
 # import os
 import sys
 
-from aux import *
+from itertools import izip
+
+from utils import *
 
 
 def _examine_oligs_set(args):
     """
-    Validate assemblies oligssetswise and show redundant oligs - whether they are main or mutant (if any).
+    Validate assemblies oligssetswise, show redundant oligs and exclude them.
     :param args: list of str
     :return: None
     """
@@ -44,13 +46,16 @@ def _examine_oligs_set(args):
 
     used_oligs = list(set(reduce(lambda res, item: res + map(get_rec_seq, item.components), oligs_sets, [])))
     print 'Number of used oligs: {}'.format(len(used_oligs))
+    mutant_oligs = filter(lambda x: x not in oligs, mutant_oligs)
     if len(used_oligs) < len(oligs) + len(mutant_oligs):
         red_oligs = filter(lambda x: x not in used_oligs, oligs)
         red_mutant_oligs = filter(lambda x: x not in used_oligs, mutant_oligs)
         if len(red_oligs) > 0:
             print 'Redundant main oligs:\n{}'.format('\n'.join(red_oligs))
+            write_fasta(oligs_fn, filter(lambda x: get_rec_seq(x) not in red_oligs, read_fasta(oligs_fn)))
         if len(red_mutant_oligs) > 0:
             print 'Redundant mutant oligs:\n{}'.format('\n'.join(red_mutant_oligs))
+            write_fasta(mutant_oligs_fn, filter(lambda x: get_rec_seq(x) not in red_mutant_oligs, read_fasta(mutant_oligs_fn)))
     used_old_oligs = filter(lambda x: x in used_oligs, old_oligs)
     used_old_mutant_oligs = filter(lambda x: x in used_oligs, old_mutant_oligs)
     print 'Number of used old oligs: {}'.format(len(used_old_oligs))
@@ -59,10 +64,46 @@ def _examine_oligs_set(args):
     print '\n'
 
 
+def _final_set_improvement(args):
+    """
+    - minimize number of main/mutant oligs
+    - validate if all assemblies are correct
+    :param args: list of str
+    :return: None
+    """
+    oligs_fn = args[0]
+    mut_fn = args[1]
+    by_oligs_pn = args[2]
+    variants_fn = args[3]
+    set_pn = os.path.abspath(by_oligs_pn)
+
+    oligs = map(get_rec_seq, read_fasta(oligs_fn))
+    mut_oligs = map(get_rec_seq, read_fasta(mut_fn))
+    oligs_sets = map(lambda x: SourceSet(x, read_fasta(x)), find_all_fasta(by_oligs_pn))
+    variants = map(get_rec_seq, read_fasta(variants_fn))
+
+    print 'Set directory: {}'.format(set_pn)
+    print 'Original number of main oligs: {}'.format(len(oligs))
+    print 'Original number of mutant oligs: {}'.format(len(mut_oligs))
+
+    mut_oligs = filter(lambda x: x not in oligs, mut_oligs)
+    used_oligs = list(set(reduce(lambda res, item: res + map(get_rec_seq, item.components), oligs_sets, [])))
+    assemblies = map(lambda x: x[0], map(assemble_chain, map(get_attr('components'), oligs_sets)))
+    assert len(oligs) == len(set(oligs))
+    assert len(mut_oligs) == len(set(mut_oligs))
+    assert len(oligs) + len(mut_oligs) == len(used_oligs)
+    assert len(assemblies) == len(variants)
+    assert all(s in variants for s in assemblies)
+    assert all(s in assemblies for s in variants)
+    assert len(assemblies) == len(set(assemblies))
+
+
 def main(args):
     option = args[0]
     if option == '--examine_set':
         return _examine_oligs_set(args[1:])
+    if option == '--improve':
+        return _final_set_improvement(args[1:])
     raise KeyError('Unknown option')
 
 
--- scripts.py
+++ scripts.py
@@ -1,6 +1,6 @@
 import sys
 
-from aux import *
+from utils import *
 
 
 def _enlarge_last_oligs(args):
@@ -9,22 +9,62 @@ def _enlarge_last_oligs(args):
     :param args: list of str
     :return: None
     """
+    min_len, max_len = 55, 59
     by_oligs_pn = args[0]
     oligs_fn = args[1]
     mutant_oligs_fn = args[2]
     variants_fn = args[3]
+    set_pn = os.path.abspath(by_oligs_pn)
 
+    print 'Processing oligs sets path: {}'.format(set_pn)
     oligs_sets = map(lambda x: SourceSet(x, read_fasta(x)), find_all_fasta(by_oligs_pn))
-    oligs = map(get_rec_seq, read_fasta(oligs_fn))
-    mut_oligs = map(get_rec_seq, read_fasta(mutant_oligs_fn))
+    oligs = read_fasta(oligs_fn)
+    mut_oligs = read_fasta(mutant_oligs_fn)
     variants = map(get_rec_seq, read_fasta(variants_fn))
 
     for olig_set in oligs_sets:
         components = olig_set.components
+        _, na_chain = assemble_chain(components)
+        if len(components[-2]) < min_len:
+            comp_str, comp_name = get_rec_seq(components[-2]), components[-2].id
+            ind = na_chain.find(comp_str)
+            if ind < 0:
+                raise ValueError('Cannot find olig in sequence.')
+            new_comp_str = na_chain[ind:ind + max_len]
+            components[-2] = SeqRecord(Seq(new_comp_str), id=comp_name, name=comp_name, description='')
+            if comp_str in map(get_rec_seq, oligs) and new_comp_str not in map(get_rec_seq, oligs):
+                oligs.append(components[-2])
+                print '\tAppended to main oligs.'
+            if comp_str in map(get_rec_seq, mut_oligs) and new_comp_str not in map(get_rec_seq, mut_oligs):
+                mut_oligs.append(components[-2])
+                print '\tAppended to mutant oligs.'
+            if assemble_chain(components)[0] not in variants:
+                raise ValueError('Could not assemble chain with new oligs.')
+        if len(components[-1]) < min_len:
+            comp_str, comp_name = get_rec_seq(components[-1]), components[-1].id
+            ind = get_rev_compl2(na_chain).find(comp_str)
+            if ind < 0:
+                raise ValueError('Cannot find olig in sequence.')
+            new_comp_str = get_rev_compl2(na_chain)[ind:ind + max_len]
+            components[-1] = SeqRecord(Seq(new_comp_str), id=comp_name, name=comp_name, description='')
+            if comp_str in map(get_rec_seq, oligs) and new_comp_str not in map(get_rec_seq, oligs):
+                oligs.append(components[-1])
+                print '\tAppended to main oligs.'
+            if comp_str in map(get_rec_seq, mut_oligs) and new_comp_str not in map(get_rec_seq, mut_oligs):
+                mut_oligs.append(components[-1])
+                print '\tAppended to mutant oligs.'
+            if assemble_chain(components)[0] not in variants:
+                raise ValueError('Could not assemble chain with new oligs.')
+        write_fasta(olig_set.source_name, components)
+    write_fasta(oligs_fn, oligs)
+    write_fasta(mutant_oligs_fn, mut_oligs)
+    print '\n'
 
 
 def main(args):
-    raise NotImplementedError()
+    option = args[0]
+    if option == '--enlarge':
+        return _enlarge_last_oligs(args[1:])
 
 
 if __name__ == '__main__':
